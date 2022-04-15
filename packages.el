8c500fa5b0f04101456e1a1674b4e4a59ecad937
Paco Pascal <me@pacopascal.com>
2022-04-14 23:26:26 -0400

    Install "elfeed-org" and "org-bullets".
    The packages "dash" and "s" are dependencies of "elfeed-org".

--- packages.el
+++ packages.el
@@ -7,7 +7,16 @@
   "362bbe5b38353d033c5299f621fea39e2c75a5e0"  ; 3.4.0
   :removal '("^web$"))
  ("youtube-dl" "https://github.com/skeeto/youtube-dl-emacs"
-  "af877b5bc4f01c04fccfa7d47a2c328926f20ef4")) ; 2018-10-12T15:08:06Z
+  "af877b5bc4f01c04fccfa7d47a2c328926f20ef4") ; 2018-10-12T15:08:06Z
+ ("org-bullets" "https://github.com/sabof/org-bullets.git"
+  "b70ac2ec805bcb626a6e39ea696354577c681b36")
+ ("dash" "https://github.com/magnars/dash.el.git"
+  "dc61f4641779616122692e34a32ba2a158ee034c")
+ ("s" "https://github.com/magnars/s.el.git"
+  "08661efb075d1c6b4fa812184c1e5e90c08795a9")
+ ("elfeed-org" "https://github.com/remyhonig/elfeed-org.git"
+  "268efdd0121fa61f63b722c30e0951c5d31224a4"
+  :removal '("Cask" "Makefile" "README.md"))) ; Depends: dash s
 
 ;; Set up some extra load-path directories
 (add-to-list 'load-path (gpkg-path "lib"))
