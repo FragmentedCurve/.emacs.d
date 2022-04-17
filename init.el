621bab66856413ac5e3ebe33f01b70809d525171
Paco Pascal <me@pacopascal.com>
2022-04-17 12:55:26 -0400

    Better support for tabs only c-mode.

--- init.el
+++ init.el
@@ -266,6 +266,26 @@
                    (interactive)
                    (term shell-file-name))))
 
+(use-package c-mode ; Is this the correct package name?
+  ;; Reference: https://www.kernel.org/doc/html/v4.10/process/coding-style.html#you-ve-made-a-mess-of-it
+  :init
+  (defun c-lineup-arglist-tabs-only (ignored)
+    "Line up argument lists by tabs, not spaces"
+    (let* ((anchor (c-langelem-pos c-syntactic-element))
+           (column (c-langelem-2nd-pos c-syntactic-element))
+           (offset (- (1+ column) anchor))
+           (steps (floor offset c-basic-offset)))
+      (* (max steps 1)
+	 c-basic-offset)))
+  :hook  (c-mode-common . (lambda ()
+				  ;; Add kernel style
+				  (c-add-style
+				   "linux-tabs-only"
+				   '("linux" (c-offsets-alist
+					      (arglist-cont-nonempty
+					       c-lineup-gcc-asm-reg
+					       c-lineup-arglist-tabs-only)))))))
+
 (use-package go-mode :ensure t)
 (use-package calfw-org :ensure t)
 
