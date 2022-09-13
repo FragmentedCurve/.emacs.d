4f585cb217b24775508670039ac4752ab7b3d318
Paco Pascal <me@pacopascal.com>
2022-09-13 04:48:51 -0400

    Install fasm-mode

--- packages.el
+++ packages.el
@@ -7,7 +7,9 @@
   "362bbe5b38353d033c5299f621fea39e2c75a5e0"  ; 3.4.0
   :removal '("^web$"))
  ("youtube-dl" "https://github.com/skeeto/youtube-dl-emacs"
-  "af877b5bc4f01c04fccfa7d47a2c328926f20ef4")) ; 2018-10-12T15:08:06Z
+  "af877b5bc4f01c04fccfa7d47a2c328926f20ef4") ; 2018-10-12T15:08:06Z
+ ("fasm-mode" "https://github.com/the-little-language-designer/fasm-mode.git"
+  "cb4b9bf48c7f05530e140456eb1f4a54716470f9"))
 
 ;; Set up some extra load-path directories
 (add-to-list 'load-path (gpkg-path "lib"))
