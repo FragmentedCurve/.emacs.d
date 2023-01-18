a2a9af9ff39a63e96325d9dff17699c61615a046
Paco Pascal <me@pacopascal.com>
2023-01-18 02:34:55 -0500

    Key bindings for moving lines & paragraphs around.

--- etc/unannoy.el
+++ etc/unannoy.el
@@ -54,8 +54,8 @@
 (define-key global-map [(control insert)] 'overwrite-mode)
 
 ;; I hate hitting this by accident
-(global-set-key (kbd "C-<up>") #'previous-line)
-(global-set-key (kbd "C-<down>") #'next-line)
+;(global-set-key (kbd "C-<up>") #'previous-line)
+;(global-set-key (kbd "C-<down>") #'next-line)
 
 ;; Magit is the only front-end I care about
 (setf vc-handled-backends nil
@@ -68,6 +68,62 @@
 
 ;; Show line numbers
 (global-linum-mode 1)
+
+(defun current-line-empty-p ()
+  (save-excursion
+    (beginning-of-line)
+    (looking-at-p "[[:blank:]]*$")))
+
+(defun line-offset ()
+  (- (point) (line-beginning-position)))
+
+(defun paragraph-offset ()
+  ;; The current line is all whitespace so jump to the beginning for
+  ;; correct offests.
+  (when (current-line-empty-p)
+    (beginning-of-line))
+
+    (let ((start-point (point)))
+      (backward-paragraph)
+      (let ((offset (- start-point (point))))
+	(goto-char (+ (point) offset))
+	offset)))
+
+;; Move lines up and down.
+;; FIXME: Moving lines up with multiple cursors is broken.
+(global-set-key (kbd "M-<up>") (lambda ()
+				 "Move current line up."
+				 (interactive)
+				 (let ((offset (line-offset)))
+				   (transpose-lines 1)
+				   (forward-line -2)
+				   (goto-char (+ (point) offset)))))
+
+(global-set-key (kbd "M-<down>") (lambda ()
+				   "Move current line down."
+				   (interactive)
+				   (let ((offset (line-offset)))
+				     (forward-line 1)
+				     (transpose-lines 1)
+				     (forward-line -1)
+				     (goto-char (+ (point) offset)))))
+
+;; Move paragraphs up and down.
+(global-set-key (kbd "M-S-<up>") (lambda ()
+				   "Move paragraph line up."
+				   (interactive)
+				   (let ((offset (paragraph-offset)))
+				     (transpose-paragraphs -1)
+				     (backward-paragraph)
+				     (goto-char (+ (point) offset)))))
+
+(global-set-key (kbd "M-S-<down>") (lambda ()
+				     "Move paragraph line down."
+				     (interactive)
+				     (let ((offset (paragraph-offset)))
+				       (transpose-paragraphs 1)
+				       (backward-paragraph)
+				       (goto-char (+ (point) offset)))))
 
 (provide 'unannoy)
 
